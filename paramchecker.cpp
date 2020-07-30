cfeadedd8dd7e7734cee180ab5e68bc7e45e8238
gireeshhiremath-lab <68851247+gireeshhiremath-lab@users.noreply.github.com>
2020-07-30 16:43:57 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -14,7 +14,7 @@ std::vector<bool> vitalsAreOk(const std::vector<Measurement>& measurements) {
   for(auto t = measurements.begin(); t != measurements.end(); t++) {
     bool vitalResult = vitalCheckers[t->id]->measurementIsOk(t->measured_value);
     std::cout << "Vital-check result is " << vitalResult << std::endl;
-    result.push_back(vitalResult);
+    results.push_back(vitalResult);
   }
   return results;
 }
